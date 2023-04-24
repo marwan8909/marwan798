57d416a079973334595ee7bffcf767986d2186d7
marwan8909 <131682597+marwan8909@users.noreply.github.com>
2023-04-24 17:27:54 +0300

    Add files via upload

---
+++ integrator.js
@@ -0,0 +1,7 @@
+processGoogleToken({
+    "newToken": "ChAI8NGYogYQzJqkgqOQ5otmEjkAm_8Fvsq-SdWZn2PkR1HCl6-8vqgKdwpz29BkXSXaasDDGEfVMWwyH_hx0hcX6RNbEQn5I2Xc3tA",
+    "validLifetimeSecs": 300,
+    "freshLifetimeSecs": 300,
+    "1p_jar": "",
+    "pucrd": ""
+});
